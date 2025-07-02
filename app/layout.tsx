67459a9757c9912df3b1c2f72f0b482538d7869f
ENTEI <entei@MacBook-Pro-Entei.local>
2025-07-02 12:34:36 +0200

    add next analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 import "./globals.css";
 import pl from "@/locales/pl";
@@ -42,6 +43,7 @@ export default function RootLayout({
       <body>
         <PasswordProtect>{children}</PasswordProtect>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
