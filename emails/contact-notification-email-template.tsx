be83e6a8337919e3b46cd286e18121af552e0770
ENTEI <entei@MacBook-Pro-Entei.local>
2025-07-02 12:08:21 +0200

    update logo size to 500px width

--- emails/contact-notification-email-template.tsx
+++ emails/contact-notification-email-template.tsx
@@ -34,8 +34,8 @@ export const ContactConfirmationEmailTemplate = ({
         <Section style={box}>
           <Img
             src={`https://rem-met.vercel.app/images/logos/rem-met-logo-light-mode.png`}
-            width="120"
-            height="20"
+            width="540"
+            height="87"
             alt="REM-MET"
           />
           <Hr style={hr} />
