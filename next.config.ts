f540aea1a2e817dca57e693d1ce8ea5ce31326be
ENTEI <entei@MacBook-Pro-Entei.local>
2025-07-03 10:37:28 +0200

    remove i18n config from next.config.ts due to incompability with approuter

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  i18n: {
-    locales: ["pl"],
-    defaultLocale: "pl",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
