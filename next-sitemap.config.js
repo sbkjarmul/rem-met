0eba3df2f6a416f8c1cfe1844adf237b1f040b00
ENTEI <entei@MacBook-Pro-Entei.local>
2025-07-04 10:48:34 +0200

    add sitemap i robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,33 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: "https://www.rem-met.com",
+  generateRobotsTxt: true,
+  exclude: ["/admin", "/private", "/koszyk", "/konto"],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/private/", "/admin/"],
+      },
+    ],
+    additionalSitemaps: ["https://www.rem-met.com/sitemap-products.xml"],
+  },
+  transform: async (config, path) => {
+    const priorities = {
+      "/": 1.0,
+      "/o-firmie": 0.7,
+      "/standardy-bezpieczenstwa": 0.5,
+      "/wygrodzenia-bezpieczenstwa/falcon": 0.9,
+      "/wygrodzenia-bezpieczenstwa/gecko": 0.9,
+      "/wygrodzenia-bezpieczenstwa/rhino": 0.9,
+    };
+
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: priorities[path] || config.priority,
+      lastmod: new Date().toISOString(),
+    };
+  },
+};
