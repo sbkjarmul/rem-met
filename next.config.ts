f77946b0b284751b32d88921f0c448988af892bf
ENTEI <entei@MacBook-Pro-Entei.local>
2025-07-05 16:28:46 +0200

    add swcMinify to next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/about-us",
-        destination: "/o-nas",
-        permanent: true,
-      },
-      {
-        source: "/privacy-policy",
-        destination: "/polityka-prywatnosci",
-        permanent: true,
-      },
-    ];
-  },
+  swcMinify: true,
 };
 
 export default nextConfig;
